ba7ba7d0a296a9c1f616d94c98f36ff043fdee8d
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2013-11-29 11:05:51 +0100

    Instead of proc_terminate really kill all children

--- adv_exec.php
+++ adv_exec.php
@@ -57,11 +57,26 @@ function adv_exec($cmd, $cwd=null, $env=null, $options=array()) {
     }
   }
 
-  if($timestamp_terminate <= time()) {
-    proc_terminate($proc);
-    $ret[0]="timeout";
+  $status = proc_get_status($proc);
+  if($status['running'] == true) {
+    // from: http://us3.php.net/manual/en/function.proc-terminate.php#81353
+    // close pipes by hand
+    fclose($pipes[1]); //stdout
+    fclose($pipes[2]); //stderr
+
+    // get the parent pid of the process we want to kill
+    $ppid = $status['pid'];
+
+    // use ps to get all the children of this process, and kill them
+    $pids = preg_split('/\s+/', `ps -o pid --no-heading --ppid $ppid`);
+    foreach($pids as $pid) {
+      if(is_numeric($pid)) {
+	posix_kill($pid, 15); // 15 is the SIGTERM signal
+      }
+    }
 
     proc_close($proc);
+    $ret[0]="timeout";
   }
   else
     $ret[0]=proc_close($proc);
