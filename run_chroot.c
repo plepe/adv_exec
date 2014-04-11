f99895c9517783430d935f4a470886575f22094e
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2014-04-11 10:20:37 +0200

    run_chroot: make sure wrapper is set to suid root

--- run_chroot.c
+++ run_chroot.c
@@ -1,8 +1,14 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+  if(geteuid() != 0) {
+    printf("run_chroot is not set to suid root!\n");
+    exit(1);
+  }
+
   chdir(argv[1]);
   chroot(argv[1]);
   seteuid(getuid());
