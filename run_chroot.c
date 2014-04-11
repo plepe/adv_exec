59764aa9f1b2250c44ea9544e3d9ef7a40bfceca
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2014-04-11 11:05:47 +0200

    run_chroot: check existance of parameters

--- run_chroot.c
+++ run_chroot.c
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+  if(argc < 2) {
+    printf("Usage: run_chroot PATH EXECUTABLE PARAMETERS\n");
+    exit(1);
+  }
+
   if(geteuid() != 0) {
     printf("run_chroot is not set to suid root!\n");
     exit(1);
