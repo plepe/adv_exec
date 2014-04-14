2e606df83e0117abeacedb8f666e9a33ae12871b
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2014-04-14 09:35:05 +0200

    Rewrote to use a class; function adv_exec() still available for compatibility

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-Like system() but with advanced options
+Like system() but with advanced options. Can be used either as class or function
 
+== Class ==
 Usage:
 ```php
-adv_exec($cmd, [$cwd], [$env], [$options]);
+$x = new AdvExec([$env], [$options]);
+$x->exec($cmd, [$cwd]);
 ```
-
-The following options are available:
-* timeout: when timeout is exceeded terminate process
 
 Return:
 ```php
@@ -15,3 +14,25 @@ array(result, "stdout", "stderr");
 
 result is exitcode or 'timeout' when execution needed to be terminated
 
+=== Available Methods ===
+| Method | Parameters | Return value | Description
+|--------|------------|--------------|-------------
+| exec | cmd, [cwd] | array(result, stdout, stderr) | Execute a command in the specified environment; result is exitcode or 'timeout' when execution needed to be terminated
+| set_option | key, value | null | Change option to the new value; see below for a list of options
+
+== Function ==
+Usage:
+```php
+adv_exec($cmd, [$cwd], [$env], [$options]);
+```
+
+Return:
+```php
+array(result, "stdout", "stderr");
+```
+
+result is exitcode or 'timeout' when execution needed to be terminated
+
+== Options ==
+The following options are available:
+* timeout: when timeout is exceeded terminate process
--- adv_exec.php
+++ adv_exec.php
@@ -2,85 +2,101 @@
 if(!isset($adv_exec_default_options))
   $adv_exec_default_options = array();
 
-// like system(), but returns:
-// array(result, "stdout", "stderr");
-function adv_exec($cmd, $cwd=null, $env=null, $options=array()) {
-  global $adv_exec_default_options;
+class AdvExec {
+  function __construct($env=null, $options=array())  {
+    $this->env = $env;
+    $this->options = $options;
 
-  // copy default options into options
-  foreach($adv_exec_default_options as $k=>$v)
-    if(!isset($options[$k]))
-      $options[$k] = $v;
+    // copy default options into options
+    global $adv_exec_default_options;
+    foreach($adv_exec_default_options as $k=>$v)
+      if(!isset($this->options[$k]))
+	$this->options[$k] = $v;
 
-  $descriptors=array(
-    1=>array("pipe", "w"),
-    2=>array("pipe", "w"),
-  );
+  }
 
-  $cmd=implode("\n", explode("\r\n", $cmd));
-  $proc=proc_open($cmd, $descriptors, $pipes, $cwd, $env);
-  if(!is_resource($proc))
-    return false;
+  function set_option($option, $value) {
+    $this->options[$option] = $value;
+  }
 
-  $ret=array(
-    "",
-    "",
-    "",
-  );
+  function exec($cmd, $cwd=null) {
+    $descriptors=array(
+      1=>array("pipe", "w"),
+      2=>array("pipe", "w"),
+    );
 
-  $orig_streams=array(
-    'read'=>array($pipes[1], $pipes[2]),
-    'write'=>array(),
-    'except'=>array()
-  );
-  $done=false;
+    $cmd=implode("\n", explode("\r\n", $cmd));
+    $proc=proc_open($cmd, $descriptors, $pipes, $cwd, $this->env);
+    if(!is_resource($proc))
+      return false;
 
-  $timestamp_terminate = null;
-  if (isset($options['timeout']))
-    $timestamp_terminate = time() + $options['timeout'];
+    $ret=array(
+      "",
+      "",
+      "",
+    );
 
-  while((!$done) &&
-        ($timestamp_terminate === null || $timestamp_terminate > time())) {
-    $streams=$orig_streams;
-    $time_left_till_terminate = ($timestamp_terminate === null ? 60 : $timestamp_terminate - time());
+    $orig_streams=array(
+      'read'=>array($pipes[1], $pipes[2]),
+      'write'=>array(),
+      'except'=>array()
+    );
+    $done=false;
 
-    stream_select(&$streams['read'], &$streams['write'], &$streams['except'], $time_left_till_terminate);
+    $timestamp_terminate = null;
+    if (isset($this->options['timeout']))
+      $timestamp_terminate = time() + $this->options['timeout'];
 
-    foreach($streams['read'] as $stream) {
-      if(feof($stream))
-	$done=true;
+    while((!$done) &&
+	  ($timestamp_terminate === null || $timestamp_terminate > time())) {
+      $streams=$orig_streams;
+      $time_left_till_terminate = ($timestamp_terminate === null ? 60 : $timestamp_terminate - time());
 
-      foreach($pipes as $i=>$pipe) {
-	if($stream==$pipe) {
-	  $ret[$i].=fgets($pipe);
+      stream_select(&$streams['read'], &$streams['write'], &$streams['except'], $time_left_till_terminate);
+
+      foreach($streams['read'] as $stream) {
+	if(feof($stream))
+	  $done=true;
+
+	foreach($pipes as $i=>$pipe) {
+	  if($stream==$pipe) {
+	    $ret[$i].=fgets($pipe);
+	  }
 	}
       }
     }
-  }
 
-  $status = proc_get_status($proc);
-  if($status['running'] == true) {
-    // from: http://us3.php.net/manual/en/function.proc-terminate.php#81353
-    // close pipes by hand
-    fclose($pipes[1]); //stdout
-    fclose($pipes[2]); //stderr
+    $status = proc_get_status($proc);
+    if($status['running'] == true) {
+      // from: http://us3.php.net/manual/en/function.proc-terminate.php#81353
+      // close pipes by hand
+      fclose($pipes[1]); //stdout
+      fclose($pipes[2]); //stderr
 
-    // get the parent pid of the process we want to kill
-    $ppid = $status['pid'];
+      // get the parent pid of the process we want to kill
+      $ppid = $status['pid'];
 
-    // use ps to get all the children of this process, and kill them
-    $pids = preg_split('/\s+/', `ps -o pid --no-heading --ppid $ppid`);
-    foreach($pids as $pid) {
-      if(is_numeric($pid)) {
-	posix_kill($pid, 15); // 15 is the SIGTERM signal
+      // use ps to get all the children of this process, and kill them
+      $pids = preg_split('/\s+/', `ps -o pid --no-heading --ppid $ppid`);
+      foreach($pids as $pid) {
+	if(is_numeric($pid)) {
+	  posix_kill($pid, 15); // 15 is the SIGTERM signal
+	}
       }
+
+      proc_close($proc);
+      $ret[0]="timeout";
     }
+    else
+      $ret[0]=$status['exitcode'];
 
-    proc_close($proc);
-    $ret[0]="timeout";
+    return $ret;
   }
-  else
-    $ret[0]=$status['exitcode'];
+}
 
-  return $ret;
+// like system(), but returns:
+// array(result, "stdout", "stderr");
+function adv_exec($cmd, $cwd=null, $env=null, $options=array()) {
+  $x = new AdvExec($env, $options);
+  return $x->exec($cmd, $cwd);
 }
